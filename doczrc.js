c3996a3195f98759501a2fafec85114bef0fa20b
Oleksandr Khomenko <homes89@ukr.net>
2020-01-18 13:13:46 +0200

    Add doczrc.js

---
+++ doczrc.js
@@ -0,0 +1,19 @@
+const fontFamily = "'Montserrat', sans-serif";
+
+export default {
+  title: "UI Kit",
+  description: "UI Kit - Collection of UI components",
+  themeConfig: {
+    styles: {
+      h1: {
+        fontFamily: fontFamily
+      },
+      h2: {
+        fontFamily: fontFamily
+      },
+      body: {
+        fontFamily: fontFamily
+      }
+    }
+  }
+};
